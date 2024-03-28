02e95397180cb4d67df97216053a014864a3b5eb
romain.chauveau <romain.chauveau@praxedo.com>
2024-03-28 12:25:02 +0100

    wip json-to-dto

--- yagni-examples/src/main/java/fr/yagni/core/dto/App.java
+++ yagni-examples/src/main/java/fr/yagni/core/dto/App.java
@@ -1,6 +1,17 @@
 package fr.yagni.core.dto;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class App {
     public static void main(String[] args) {
@@ -11,6 +22,22 @@ public class App {
                 .withoutPhone()
                 .build();
         System.out.println(buildedPerson);
+
+        jsonToDto();
+    }
+
+    private static void jsonToDto() {
+        ObjectMapper om = new ObjectMapper();
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        InputStream is = classloader.getResourceAsStream("request.json");
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+            String text = reader.lines().collect(Collectors.joining("\n"));
+            Map<String, Object> map = om.readValue(text, new TypeReference<>() {});
+            System.out.println(map);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Builder
